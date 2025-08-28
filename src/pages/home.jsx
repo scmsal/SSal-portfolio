c4894882875d61afb2d27deb30731f7c379397c3
scmsal <31stephcm@gmail.com>
2025-08-28 15:15:34 -0400

    made layout responsive for mobile screens

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,29 +4,29 @@ import NavBar from "../components/NavBar";
 const HomePage = () => {
   return (
     <main className="flex flex-grow flex-col justify-center items-start">
-      <div className="flex px-8 pt-8 ">
-        <div className="flex flex-col items-start justify-center w-2/3">
+      <div className="grid grid-cols-2 px-4 md:px-8 pt-4 md:pt-8 ">
+        <div className="col-span-2  md:col-span-1">
           <div>
-            <span className="font-bold uppercase text-6xl font-upper text-transparent bg-clip-text bg-linear-to-r from-cobalt from-10%  via-purple-500 via-75% to-pink-500 to-90%">
+            <span className="font-bold uppercase text-4xl md:text-6xl font-upper text-transparent bg-clip-text bg-linear-to-r from-cobalt from-10%  via-purple-500 via-75% to-pink-500 to-90%">
               Frontend developer
             </span>
           </div>
-          <div className=" text-xl w-1/2">
+          <div className="text-lg md:text-xl mb-5">
             <div className="font-bold mb-4">
               with full stack exposure and a growth mindset
             </div>
             <p className="text-gray-500">
               Focused on building clean, responsive UIs with React and Tailwind.
-              Actively deepening skills in backend technologies, testing and
-              internationalization.
+              Actively deepening skills in frontend and backend technologies,
+              testing, and internationalization.
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center w-1/3">
+        <div className="col-span-2 md:col-span-1 justify-items-end ">
           <img
             src="/project_imgs/wordcloud-2.png"
             alt="developer word cloud"
-            className="object-cover w-full h-full"
+            className="object-cover fluid w-75 lg:w-100"
           />
         </div>
       </div>
